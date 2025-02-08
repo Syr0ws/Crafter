b528ba94ad1a5cdf869ee85078e8932b53a395e3
Syrows <clement.gho@gmail.com>
2025-02-08 20:01:54 +0100

    adding missing javadoc

--- src/main/java/com/github/syr0ws/crafter/util/Observer.java
+++ src/main/java/com/github/syr0ws/crafter/util/Observer.java
@@ -1,6 +1,12 @@
 package com.github.syr0ws.crafter.util;
 
+/**
+ * Represents an observer that gets notified of changes.
+ */
 public interface Observer {
 
+    /**
+     * Notifies the observer of a change.
+     */
     void notifyChange();
 }
