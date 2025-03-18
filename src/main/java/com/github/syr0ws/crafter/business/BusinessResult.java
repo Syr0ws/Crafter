189435a649fe00494a21dde09fa1096ace9ca031
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2025-03-18 19:40:53 +0100

    feat: adding reusable classes to implement the business service layer (#34)

---
+++ src/main/java/com/github/syr0ws/crafter/business/BusinessResult.java
@@ -0,0 +1,46 @@
+package com.github.syr0ws.crafter.business;
+
+import com.github.syr0ws.crafter.util.Validate;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class BusinessResult<T, F extends BusinessFailure> {
+
+    private final T result;
+    private final F failure;
+
+    private BusinessResult(T result, F failure) {
+        this.result = result;
+        this.failure = failure;
+    }
+
+    public static <T, F extends BusinessFailure> BusinessResult<T, F> success(T result) {
+        return new BusinessResult<>(result, null);
+    }
+
+    public static <T, F extends BusinessFailure> BusinessResult<T, F> error(F failure) {
+        Validate.notNull(failure, "failure cannot be null");
+        return new BusinessResult<>(null, failure);
+    }
+
+    public BusinessResult<T, F> onSuccess(Consumer<T> consumer) {
+        Validate.notNull(consumer, "consumer cannot be null");
+        this.success().ifPresent(consumer);
+        return this;
+    }
+
+    public BusinessResult<T, F> onFailure(Consumer<F> consumer) {
+        Validate.notNull(consumer, "consumer cannot be null");
+        this.failure().ifPresent(consumer);
+        return this;
+    }
+
+    public Optional<T> success() {
+        return Optional.ofNullable(this.result);
+    }
+
+    public Optional<F> failure() {
+        return Optional.ofNullable(this.failure);
+    }
+}
