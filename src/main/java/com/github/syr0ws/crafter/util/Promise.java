d4c7d26ad5782c379d03198d1230ba4f3762b356
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2025-03-18 19:41:06 +0100

    feat: adding ability to register several callbacks (#35)

--- src/main/java/com/github/syr0ws/crafter/util/Promise.java
+++ src/main/java/com/github/syr0ws/crafter/util/Promise.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.function.Consumer;
 
 /**
@@ -14,10 +16,13 @@ import java.util.function.Consumer;
 public class Promise<T> {
 
     private final PromiseExecutor<T> executor;
+    private final Queue<Consumer<T>> thens = new ConcurrentLinkedDeque<>();
+    private final Queue<Consumer<Throwable>> excepts = new ConcurrentLinkedDeque<>();
+    private final Queue<Runnable> completes = new ConcurrentLinkedDeque<>();
 
-    private Consumer<T> then;
-    private Consumer<Throwable> except;
-    private Runnable complete;
+    private T value;
+    private Throwable throwable;
+    private PromiseStatus status = PromiseStatus.PENDING;
 
     /**
      * Constructs a new {@code Promise} with the specified executor.
@@ -31,41 +36,71 @@ public class Promise<T> {
     }
 
     /**
-     * Registers a callback to be executed when the promise resolves successfully.
+     * Registers a callback to be executed when the promise is fulfilled.
      *
-     * @param consumer the consumer to handle the resolved value.
-     * @return this promise instance.
-     * @throws IllegalArgumentException if the consumer is {@code null}.
+     * <p>If the promise is in the {@code PromiseStatus.PENDING} state, the provided callback
+     * is queued and will be executed when the promise transitions to {@code PromiseStatus.FULFILLED}.
+     * If the promise is already in the {@code PromiseStatus.FULFILLED} state, the callback is executed immediately.</p>
+     *
+     * @param consumer the callback to handle the fulfilled value
+     * @return this promise instance
+     * @throws IllegalArgumentException if {@code consumer} is {@code null}
      */
     public Promise<T> then(Consumer<T> consumer) {
         Validate.notNull(consumer, "consumer cannot be null");
-        this.then = consumer;
+
+        if (this.status == PromiseStatus.PENDING) {
+            this.thens.offer(consumer);
+        } else if (this.status == PromiseStatus.FULFILLED) {
+            consumer.accept(this.value);
+        }
+
         return this;
     }
 
     /**
-     * Registers a callback to be executed when an exception occurs.
+     * Registers a callback to be executed when the promise encounters an exception.
+     *
+     * <p>If the promise is in the {@code PromiseStatus.PENDING} state, the provided callback
+     * is queued and will be executed when the promise transitions to {@code PromiseStatus.REJECTED}.
+     * If the promise is already in the {@code PromiseStatus.REJECTED} state, the callback is executed immediately.</p>
      *
      * @param consumer the consumer to handle the exception.
      * @return this promise instance.
-     * @throws IllegalArgumentException if the consumer is {@code null}.
+     * @throws IllegalArgumentException if the {@code consumer} is {@code null}.
      */
     public Promise<T> except(Consumer<Throwable> consumer) {
         Validate.notNull(consumer, "consumer cannot be null");
-        this.except = consumer;
+
+        if (this.status == PromiseStatus.PENDING) {
+            this.excepts.offer(consumer);
+        } else if (this.status == PromiseStatus.REJECTED) {
+            consumer.accept(this.throwable);
+        }
+
         return this;
     }
 
     /**
-     * Registers a callback to be executed when the promise is completed.
+     * Registers a callback to be executed when the promise is completed, regardless of its outcome.
      *
-     * @param runnable the runnable to execute upon completion.
-     * @return this promise instance.
-     * @throws IllegalArgumentException if the runnable is {@code null}.
+     * <p>If the promise is in the {@code PromiseStatus.PENDING} state, the provided callback
+     * is queued and will be executed when the promise is resolved (either fulfilled or rejected).
+     * If the promise is already resolved, the callback is executed immediately.</p>
+     *
+     * @param runnable the callback to execute upon completion
+     * @return this promise instance
+     * @throws IllegalArgumentException if {@code runnable} is {@code null}
      */
     public Promise<T> complete(Runnable runnable) {
         Validate.notNull(runnable, "runnable cannot be null");
-        this.complete = runnable;
+
+        if (this.status == PromiseStatus.PENDING) {
+            this.completes.offer(runnable);
+        } else {
+            runnable.run();
+        }
+
         return this;
     }
 
@@ -74,15 +109,19 @@ public class Promise<T> {
      *
      * <p>This method executes the resolution logic immediately on the current thread
      * and does not use a Bukkit scheduling method.</p>
-     *
-     * @throws IllegalArgumentException if the plugin is {@code null}.
      */
     public void resolve() {
+
+        if (this.status != PromiseStatus.PENDING) {
+            throw new IllegalStateException("Promise has already been resolved");
+        }
+
         try {
             this.executor.execute(this::onThen, this::onExcept);
         } catch (Exception exception) {
             this.onExcept(exception);
         }
+
         this.onComplete();
     }
 
@@ -90,7 +129,7 @@ public class Promise<T> {
      * Resolves the promise synchronously in a Bukkit task.
      *
      * <p>This method schedules the resolution logic to run synchronously on the main
-     * server thread using {@link BukkitScheduler#runTask(Plugin, Runnable)}.</p>
+     * server thread the next tick using {@link BukkitScheduler#runTask(Plugin, Runnable)}.</p>
      *
      * @param plugin the plugin instance required to schedule the task.
      * @throws IllegalArgumentException if the plugin is {@code null}.
@@ -114,21 +153,43 @@ public class Promise<T> {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, this::resolve);
     }
 
+    /**
+     * Returns the current status of the promise.
+     *
+     * @return the current {@code PromiseStatus} of this promise
+     */
+    public PromiseStatus getStatus() {
+        return this.status;
+    }
+
     private void onThen(T value) {
-        if(this.then != null) {
-            this.then.accept(value);
+        this.status = PromiseStatus.FULFILLED;
+        this.value = value;
+
+        while (!this.thens.isEmpty()) {
+            Consumer<T> consumer = this.thens.poll();
+            consumer.accept(value);
         }
     }
 
     private void onExcept(Throwable throwable) {
-        if(this.except != null) {
-            this.except.accept(throwable);
+        this.status = PromiseStatus.REJECTED;
+        this.throwable = throwable;
+
+        while (!this.excepts.isEmpty()) {
+            Consumer<Throwable> consumer = this.excepts.poll();
+            consumer.accept(throwable);
         }
     }
 
     private void onComplete() {
-        if(this.complete != null) {
-            this.complete.run();
+        while (!this.completes.isEmpty()) {
+            Runnable runnable = this.completes.poll();
+            runnable.run();
         }
+    }
+
+    public enum PromiseStatus {
+        PENDING, FULFILLED, REJECTED
     }
 }
