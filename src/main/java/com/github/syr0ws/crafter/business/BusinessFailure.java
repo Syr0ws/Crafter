b4fb83ffc5a3a6151ecf32b5832e60ca9eb5c33d
Syrows <clement.gho@gmail.com>
2025-04-09 19:35:31 +0200

    docs: adding javadoc on business types

--- src/main/java/com/github/syr0ws/crafter/business/BusinessFailure.java
+++ src/main/java/com/github/syr0ws/crafter/business/BusinessFailure.java
@@ -1,5 +1,11 @@
 package com.github.syr0ws.crafter.business;
 
+/**
+ * Represents a failure that occurred during the execution of a business operation.
+ *
+ * <p> This interface can be implemented to provide details about the specific cause
+ * or type of business failure.</p>
+ */
 public interface BusinessFailure {
 
 }
--- src/main/java/com/github/syr0ws/crafter/business/BusinessFailureHandler.java
+++ src/main/java/com/github/syr0ws/crafter/business/BusinessFailureHandler.java
@@ -5,9 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a method as a handler for a specific type of {@link BusinessFailure}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface BusinessFailureHandler {
 
+    /**
+     * Specifies the type of {@link BusinessFailure} this method handles.
+     *
+     * @return the class type of the business failure
+     */
     Class<? extends BusinessFailure> type();
 }
