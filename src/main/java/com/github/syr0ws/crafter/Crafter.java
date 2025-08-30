0db66d005a1baf13a511526ea62ec525882744f0
Syrows <clement.gho@gmail.com>
2025-08-30 18:13:51 +0200

    wip

--- src/main/java/com/github/syr0ws/crafter/Crafter.java
+++ src/main/java/com/github/syr0ws/crafter/Crafter.java
@@ -1,16 +1,11 @@
 package com.github.syr0ws.crafter;
 
 import com.github.syr0ws.crafter.database.DatabaseService;
+import com.github.syr0ws.crafter.database.SimpleDatabaseService;
 import com.github.syr0ws.crafter.database.config.loader.DatabaseConfigService;
-import com.github.syr0ws.crafter.database.config.loader.RemoteDatabaseConfigLoader;
-import com.github.syr0ws.crafter.database.config.loader.SQLiteConfigurationLoader;
 import com.github.syr0ws.crafter.database.config.loader.YamlDatabaseConfigService;
 import com.github.syr0ws.crafter.database.driver.DatabaseDriver;
 import com.github.syr0ws.crafter.database.driver.SupportedDriverList;
-import com.github.syr0ws.crafter.database.provider.MariaDBDatabaseProvider;
-import com.github.syr0ws.crafter.database.provider.MySQLDatabaseProvider;
-import com.github.syr0ws.crafter.database.provider.PostgreSQLDatabaseProvider;
-import com.github.syr0ws.crafter.database.provider.SQLiteDatabaseProvider;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class Crafter {
@@ -28,13 +23,6 @@ public class Crafter {
     }
 
     public static DatabaseService createDatabaseService() {
-
-        DatabaseService service = new DatabaseService();
-        service.addProvider(new SQLiteDatabaseProvider());
-        service.addProvider(new MySQLDatabaseProvider());
-        service.addProvider(new MariaDBDatabaseProvider());
-        service.addProvider(new PostgreSQLDatabaseProvider());
-
-        return service;
+        return new SimpleDatabaseService();
     }
 }
