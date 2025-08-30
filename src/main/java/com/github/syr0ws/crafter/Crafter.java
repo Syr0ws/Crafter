e4f2d707b51cb5323640de438f48424bd63901ce
Syrows <clement.gho@gmail.com>
2025-08-30 19:17:14 +0200

    added javadoc

--- src/main/java/com/github/syr0ws/crafter/Crafter.java
+++ src/main/java/com/github/syr0ws/crafter/Crafter.java
@@ -8,12 +8,25 @@ import com.github.syr0ws.crafter.database.driver.DatabaseDriver;
 import com.github.syr0ws.crafter.database.driver.SupportedDriverList;
 import org.bukkit.configuration.file.FileConfiguration;
 
+/**
+ * Global class with default behaviors for the Crafter library.
+ */
 public class Crafter {
 
+    /**
+     * Creates a {@link DatabaseConfigService} instance for {@link FileConfiguration}.
+     *
+     * @return a {@link DatabaseConfigService} that loads database configurations from YAML files
+     */
     public static DatabaseConfigService<FileConfiguration> createYamlDatabaseConfigService() {
         return new YamlDatabaseConfigService();
     }
 
+    /**
+     * Creates a {@link SupportedDriverList} containing all default {@link DatabaseDriver} values.
+     *
+     * @return list of default supported database drivers
+     */
     public static SupportedDriverList getDefaultSupportedDrivers() {
 
         SupportedDriverList list = new SupportedDriverList();
@@ -22,6 +35,11 @@ public class Crafter {
         return list;
     }
 
+    /**
+     * Creates a {@link DatabaseService} instance.
+     *
+     * @return a database service
+     */
     public static DatabaseService createDatabaseService() {
         return new SimpleDatabaseService();
     }
