6fbaf3d17d485a4e62824875206678cd491956dd
Syrows <clement.gho@gmail.com>
2025-12-24 16:36:28 +0100

    feat: added NMS version handling

---
+++ src/main/java/com/github/syr0ws/crafter/nms/ServerVersionNotFoundException.java
@@ -0,0 +1,21 @@
+package com.github.syr0ws.crafter.nms;
+
+/**
+ * Thrown when a corresponding {@link ServerVersion} cannot be determined.
+ */
+public class ServerVersionNotFoundException extends Exception {
+
+    /**
+     * @see Exception#Exception(String)
+     */
+    public ServerVersionNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * @see Exception#Exception(String, Throwable)
+     */
+    public ServerVersionNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
